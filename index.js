0740c93877ce98f254c3b1a794c812dacd8190f5
Carlos Aniel <89603527+carlos-aniel@users.noreply.github.com>
2022-01-20 21:41:55 -0300

    Segundo(sign up, sign in)
    Adicionando esboço do login e cadastro

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 7070;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//Routes
 app.get('/',(req, res)=>{
     res.render('home.ejs');
 });
 
+//Functions
 app.get('/publicnotes',(req, res)=>{
     res.render('publicnotes.ejs');
 });
@@ -20,6 +22,16 @@ app.get('/folders',(req, res)=>{
 app.get('/files',(req, res)=>{
     res.render('files.ejs');
 });
+
+//Sign
+app.get('/sign-in',(req, res)=>{
+    res.render('sign-in-form.ejs');
+});
+
+app.get('/sign-up',(req, res)=>{
+    res.render('sign-up-form.ejs');
+});
+
 
 try{
     app.listen(port,()=>{console.log(`Servidor rodando na porta ${port}`)});
